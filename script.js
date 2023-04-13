3b709fd1c2656c47fd94506f68826e3c161edd53
Afzal Ahmad <113641536+afzalahmad1@users.noreply.github.com>
2023-04-14 00:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this._species = species;
+	}
+	get species(){
+		return this._species;
+	}
+	makeSound(){
+		console.log(`The ${this._species} makes a sound`)
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(){
+		super(species)
+	}
+	bark(){
+		console.log(`The ${this._species} barks.`)
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(){
+		super(species)
+	}
+	purr(){
+		console.log(`The ${this._species} purrs.`)
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
